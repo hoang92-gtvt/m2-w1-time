2419b48b74e35046aed44c7dfa1ef077a1e7a417
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-04-02 13:47:00 +0700

    'upfile'

---
+++ src/Time.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Time {
+    public static void main (String[] args) {
+        Date now = new Date();
+        System.out.println( "Bây giờ là:"+ now);
+    }
+
+}
